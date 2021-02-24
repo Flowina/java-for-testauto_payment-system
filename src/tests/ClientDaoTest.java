e1c0e7d18e33416495f05401e98c6da23ce60e72
Flowina <bilobik@mail.ru>
2021-02-24 14:44:11 +0300

    tests: client

---
+++ src/tests/ClientDaoTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import dao.ClientDao;
+import dao.ClientDaoImpl;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import services.ClientService;
+
+import java.sql.SQLException;
+
+public class ClientDaoTest {
+    private ClientDao<Integer> clientDao;
+
+    @BeforeClass
+    public void beforeClass() {
+        clientDao = new ClientDaoImpl(TestSettings.connectionFactory);
+    }
+
+    @Test(expectedExceptions = { SQLException.class, NullPointerException.class })
+    public void testCreate() throws SQLException {
+        clientDao.create(null);
+    }
+
+    @Test
+    public void testGetAll() {
+    }
+
+    @Test
+    public void testFindById() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testFindByName() {
+    }
+}
--- src/tests/ClientServiceTest.java
+++ src/tests/ClientServiceTest.java
@@ -15,28 +15,71 @@ public class ClientServiceTest {
     ClientDao<Integer> clientDao;
     ClientService<Integer> clientService;
 
+    Client[] clients = {
+            new Client("Manning", "Victoria", Date.valueOf("1980-02-14")),
+            new Client("Rampling", "Cameron", Date.valueOf("1959-02-14")),
+            new Client("Davidson", "Audrey", Date.valueOf("1959-02-14")),
+            new Client("Lawrence", "Benjamin", Date.valueOf("2000-02-14")),
+            new Client("James", "Fiona", Date.valueOf("1999-02-14")),
+    };
+
     @BeforeClass
     public void beforeClass() {
         clientDao = new ClientDaoImpl(TestSettings.connectionFactory);
         clientService = new ClientService<Integer>(clientDao);
     }
 
-    @Test
+    @Test(priority=1)
     public void client_create__should_create_new_client() {
-        Client newClient = new Client("Lara", "Croft", Date.valueOf("1959-02-14"));
-        clientService.create(newClient);
-        try {
-            Client client = clientDao.findById(newClient.getId());
-            Assert.assertTrue(client != null);
-        } catch (SQLException e) {
-            Assert.fail("Error creating new client", e);
+        for (Client newClient:
+            clients) {
+            clientService.create(newClient);
+            try {
+                Client client = clientDao.findById(newClient.getId());
+                Assert.assertNotNull(client);
+            } catch (SQLException e) {
+                Assert.fail("Error creating new client", e);
+            }
         }
-}
+    }
+
 
     @Test
-    public void test(){
-        //Assert.fail("test");
-        Assert.assertTrue(true);
+    public void testCreate() {
+    }
+
+    @Test(dependsOnMethods = {"client_create__should_create_new_client",})
+    public void testUpdate_should_update_user() throws SQLException {
+        for (Client existClient:
+                clients) {
+            String newName = existClient.getLastName() + " Jr.";
+            existClient.setLastName(newName);
+            clientService.update(existClient);
+
+            Client client = clientDao.findById(existClient.getId());
+            Assert.assertNotNull(client);
+            Assert.assertEquals(client.getLastName(), newName, "Expected name = " + newName);
+        }
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testTestDelete() {
+    }
+
+    @Test
+    public void testFindById() {
+    }
+
+    @Test
+    public void testFindByName() {
+    }
+
+    @Test
+    public void testGetAll() {
     }
 }
 /*
